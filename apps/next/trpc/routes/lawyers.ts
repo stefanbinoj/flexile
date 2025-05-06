405dee161263be34edecb8db7449a19e9db0d86b
Raul Popadineti <raul@sunergos.ro>
2025-05-06 13:39:32 -0400

    Fix: Update company ID property and error handling in lawyers router invite mutation (#270)

--- apps/next/trpc/routes/lawyers.ts
+++ apps/next/trpc/routes/lawyers.ts
@@ -7,15 +7,15 @@ export const lawyersRouter = createRouter({
   invite: companyProcedure.input(z.object({ email: z.string() })).mutation(async ({ ctx, input }) => {
     if (!ctx.companyAdministrator) throw new TRPCError({ code: "FORBIDDEN" });
 
-    const response = await fetch(company_lawyers_url(ctx.company.id, { host: ctx.host }), {
+    const response = await fetch(company_lawyers_url(ctx.company.externalId, { host: ctx.host }), {
       method: "POST",
       body: JSON.stringify(input),
       headers: { "Content-Type": "application/json", ...ctx.headers },
     });
 
     if (!response.ok) {
-      const { error } = z.object({ error: z.string() }).parse(await response.json());
-      throw new TRPCError({ code: "BAD_REQUEST", message: error });
+      const { error_message } = z.object({ error_message: z.string() }).parse(await response.json());
+      throw new TRPCError({ code: "BAD_REQUEST", message: error_message });
     }
   }),
 });
