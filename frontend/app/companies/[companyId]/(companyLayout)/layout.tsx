fab8d926550ac731d93af4a6fea0a8d07588dbf7
stefanbinoj <stefan.binoj.007@gmail.com>
2025-07-14 15:28:52 +0530

    moved to dashboard group for common layout

--- frontend/app/companies/[companyId]/(companyLayout)/layout.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import MainLayout from "@/components/layouts/Main";
-
-export default function CompanyLayout({ children }: { children: React.ReactNode }) {
-  return <MainLayout>{children}</MainLayout>;
-}
